3e2d731b6baa351b73f9d4da8b3d2c277abf961e
Utsav Das <2015utsav.das@ves.ac.in>
2018-03-29 18:58:30 +0530

    Merge: Origin and local

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-socketio = SocketIO(app)
